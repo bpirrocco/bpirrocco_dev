ef2d8e1eb3cb186e97085e76d05582cc99f1b4aa
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-28 22:17:16 -0500

    Attempts at JavaScript for hiding welcome

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
-// if (window.location.pathname == '') {
-    
-// }
+function unwelcome() {
+    let el = document.getElementsByClassName("testdiv");
+    Array.from(el).forEach((el) =>
+        el.classList.add("hidden")
+    );
+}
+
+// TODO: I need to turn this HTMLCollection object into an array, so I can iterate
+// over it
+
+unwelcome();
