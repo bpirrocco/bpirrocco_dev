7a2d81d601ceb30e465a2b743fb71e5d550e26a5
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-02 20:10:17 -0400

    Primitive counter set up

---
+++ scripts/rscript.js
@@ -0,0 +1,28 @@
+// ****************
+// RESUME SCRIPTING
+// ****************
+
+
+// function visitCounter() {
+    // const body = document.getElementById("resume");
+    window.addEventListener("DOMContentLoaded", updateCounter)
+
+// }
+
+function checkCookie() {
+    let counter = getCookie("counter");
+    if (!counter) {
+        setCookie("counter", 0, 400);
+        return 0
+    } else {
+        counter++;
+        setCookie("counter", counter, 400);
+        return counter
+    }
+}
+
+function updateCounter() {
+    const counter = document.getElementById("counter");
+    let value = checkCookie();
+    counter.textContent = value;
+}
